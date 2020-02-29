1c8118496a6e56d2f396b941ce9aa401a145bf96
root <root@backend-services.us-west1-c.c.wrightway-265101.internal>
2020-02-29 11:38:41 +0000

    extension dark

--- app/etc/config.php
+++ app/etc/config.php
@@ -240,6 +240,8 @@ return [
         'Klarna_Kp' => 1,
         'Magento_PaypalReCaptcha' => 1,
         'MSP_TwoFactorAuth' => 1,
+        'MageSpices_Core' => 1,
+        'MageSpices_Mage2Dark' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1,
         'Vertex_AddressValidation' => 1,
